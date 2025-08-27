cfad1d411a78f299c0525fa138c482dcca94ffe1
lyz24 <1721250722@qq.com>
2025-08-27 15:15:36 +0800

    first commit

---
+++ my.py
@@ -0,0 +1,6 @@
+import numpy
+import torch.nn as nn
+
+class GNN():
+    def __init__(self) -> None:
+        pass
---
+++ test.py
@@ -0,0 +1,22 @@
+import numpy
+import os
+from my import GNN
+
+a = 1
+m = GNN()
+for i in range(10):
+    print(a)
+    print(a)
+    pass
+for i in range(1):
+    print()
+    print()
+    pass
+
+ll = {"1" : 1,
+      "2" : 2,
+      "3" : [1, 2, 3]
+      }
+print("hello python")
+print(m)
+print(ll)
